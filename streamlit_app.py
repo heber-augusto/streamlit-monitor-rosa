3eb3575616ed4f7b126eb6ef459e790a07ddd862
Heber Scachetti <heber.augusto@gmail.com>
2022-11-19 22:45:50 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,37 @@
 from datetime import datetime
+from io import BytesIO
 
 import streamlit as st
 from vega_datasets import data
 
 from utils import chart, db
+from google.oauth2 import service_account
+from google.cloud import storage
+import pandas as pd
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = storage.Client(credentials=credentials)
+
+
+# Retrieve file contents.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def read_file(bucket_name, file_path):
+    bucket = client.bucket(bucket_name)
+    content = bucket.blob(file_path).download_as_string().decode("utf-8")
+    return content
+
+bucket_name = "observatorio-oncologia"
+file_path = r"monitor/SP/consolidado/dados_estad_mensal.parquet.gzip"
+
+content = read_file(bucket_name, file_path)
+
+bytes_io = BytesIO(content.readall())
+df = pd.read_parquet(bytes_io)
+
 
 def space(num_lines=1):
     """Adds empty lines to the Streamlit app."""
@@ -15,7 +43,7 @@ st.set_page_config(layout="centered", page_icon="💬", page_title="Commenting a
 
 # Data visualisation part
 
-st.title("💬 Commenting app")
+st.title(f"💬 Commenting app {len(df.index)}")
 
 source = data.stocks()
 all_symbols = source.symbol.unique()
