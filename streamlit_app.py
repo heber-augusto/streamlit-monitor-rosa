f2561d172e91471ddce9beeb2d2bac9043f529c4
Heber Scachetti <heber.augusto@gmail.com>
2022-11-20 00:01:09 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,8 +73,8 @@ metrics_selector = st.selectbox(
 )
 
 
-min_date = dados_estad_mensal.data.dt.date[0]
-max_date = dados_estad_mensal.data.dt.date[-1]
+# min_date = dados_estad_mensal.data.dt.date[0]
+# max_date = dados_estad_mensal.data.dt.date[-1]
 # min_date = datetime.date(2020,1,1)
 # max_date = datetime.date(2022,1,1)
 # a_date = st.date_input("Período", (min_date, max_date))
